78772c0e8b581578295b7357f388dbe7a18788c6
Cristian Franco <cfrancobedoya@gmail.com>
2022-01-01 14:38:13 -0500

    Develop: Getting Pokemonx from API

--- src/screens/PokedexScreen.tsx
+++ src/screens/PokedexScreen.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { SafeAreaView, Text } from "react-native";
+import { getPokemonsApi } from "../api/pokemon";
 
 const PokedexScreen = () => {
+  const [state, setstate] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })();
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
