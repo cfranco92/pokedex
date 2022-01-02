5ebb368b2e5177d1b9408182a7645681d78c7b9f
Cristian Franco <cfrancobedoya@gmail.com>
2022-01-01 21:20:50 -0500

    Develop: Add navigation bar

--- src/screens/PokemonScreen.tsx
+++ src/screens/PokemonScreen.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 
 import Header from "../components/Pokemon/Header";
+import Icon from "react-native-vector-icons/FontAwesome5";
 import { Pokemon } from "../models/pokemon";
 import { ScrollView } from "react-native";
 import Stats from "../components/Pokemon/Stats";
@@ -8,7 +9,16 @@ import Type from "../components/Pokemon/Type";
 import { getPokemonDetailsApi } from "../api/pokemon";
 
 export interface PokemonScreenProps {
-  navigation: { goBack: () => void };
+  navigation: {
+    goBack: () => void;
+    setOptions: ({
+      headerRight,
+      headerLeft,
+    }: {
+      headerRight: () => void;
+      headerLeft: () => void;
+    }) => void;
+  };
   route: { params: { id: string } };
 }
 
@@ -17,6 +27,20 @@ const PokemonScreen = ({
   route: { params },
 }: PokemonScreenProps) => {
   const [pokemon, setPokemon] = useState<Pokemon | any | null>(null);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => null,
+      headerLeft: () => (
+        <Icon
+          name="arrow-left"
+          size={20}
+          style={{ marginLeft: 20 }}
+          onPress={navigation.goBack}
+        />
+      ),
+    });
+  }, [navigation, params]);
 
   useEffect(() => {
     (async () => {
