27a6f3103425eb0afd70694964aa99b69b8b0f25
Cristian Franco <cfrancobedoya@gmail.com>
2022-01-08 17:17:03 -0500

    Create LoginForm

--- src/components/Auth/LogingForm.tsx
+++ src/components/Auth/LogingForm.tsx
@@ -1,13 +1,43 @@
 import React from "react";
-import { Text, View } from "react-native";
+
+import { Button, Keyboard, StyleSheet, Text, TextInput, View } from "react-native";
 
 
 const LogingForm = () => {
   return (
     <View>
-      <Text>LogingForm...</Text>
+      <Text style={stlyes.title}>Log in...</Text>
+      <TextInput
+        placeholder="User name"
+        style={stlyes.input}
+        autoCapitalize="none"
+      />
+      <TextInput
+        placeholder="Password"
+        style={stlyes.input}
+        autoCapitalize="none"
+        secureTextEntry={true}
+      />
+      <Button title='Enter' onPress={() => console.log('Getting in...')}/>
     </View>
   );
 };
+
+const stlyes = StyleSheet.create({
+  title: {
+    textAlign: "center",
+    fontSize: 29,
+    fontWeight: "bold",
+    marginTop: 50,
+    marginBottom: 15,
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+  },
+});
 
 export default LogingForm;
